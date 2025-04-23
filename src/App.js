bf992d7f51f86bbeaccdb8c23538d8867f4b3e4e
BallaSaiBhargav <135804161+BallaSaiBhargav@users.noreply.github.com>
2025-04-23 13:47:07 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import NavBar from "./ProjectFolder/NavBar";
+import PostDisplay from "./ProjectFolder/PostDisplay";
+
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <PostDisplay />
+    </div>
+  );
+};
+
+export default App;
